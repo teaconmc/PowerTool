7866a82eacb8c3f94113e9b88f22edc6828ba58a
3TUSK <urey.s.knowledge@gmail.com>
2022-07-25 14:51:45 -0700

    Item Supplier usage description

--- src/main/java/org/teacon/powertool/block/ItemSupplierBlock.java
+++ src/main/java/org/teacon/powertool/block/ItemSupplierBlock.java
@@ -1,11 +1,15 @@
 package org.teacon.powertool.block;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
@@ -14,7 +18,10 @@ import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
 import org.teacon.powertool.block.entity.ItemSupplierBlockEntity;
+
+import java.util.List;
 
 public class ItemSupplierBlock extends BaseEntityBlock {
     public ItemSupplierBlock(Properties prop) {
@@ -39,6 +46,11 @@ public class ItemSupplierBlock extends BaseEntityBlock {
     @Override
     public boolean propagatesSkylightDown(BlockState state, BlockGetter level, BlockPos pos) {
         return true;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable BlockGetter level, List<Component> tooltip, TooltipFlag flag) {
+        tooltip.add(new TranslatableComponent("block.powertool.item_supplier.tooltip").withStyle(ChatFormatting.GRAY));
     }
 
     @Override

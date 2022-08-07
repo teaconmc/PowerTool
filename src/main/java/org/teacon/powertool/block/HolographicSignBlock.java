7ff9744d4a581fe3eff1378159e085d12483fb8a
3TUSK <urey.s.knowledge@gmail.com>
2022-08-07 16:40:25 -0700

    Make Holographic Sign waterloggable

--- src/main/java/org/teacon/powertool/block/HolographicSignBlock.java
+++ src/main/java/org/teacon/powertool/block/HolographicSignBlock.java
@@ -1,15 +1,24 @@
 package org.teacon.powertool.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.network.PacketDistributor;
 import org.jetbrains.annotations.Nullable;
@@ -17,10 +26,18 @@ import org.teacon.powertool.block.entity.HolographicSignBlockEntity;
 import org.teacon.powertool.network.PowerToolNetwork;
 import org.teacon.powertool.network.client.OpenHolographicSignEditor;
 
-public class HolographicSignBlock extends BaseEntityBlock {
+public class HolographicSignBlock extends BaseEntityBlock implements SimpleWaterloggedBlock {
 
-    protected HolographicSignBlock(Properties prop) {
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
+    public HolographicSignBlock(Properties prop) {
         super(prop);
+        this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, Boolean.FALSE));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(WATERLOGGED);
     }
 
     @Override
@@ -36,9 +53,25 @@ public class HolographicSignBlock extends BaseEntityBlock {
 
     @Override
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        if (player.isCrouching()) {
+            return InteractionResult.PASS;
+        }
         if (!level.isClientSide() && player instanceof ServerPlayer sp) {
             PowerToolNetwork.channel().send(PacketDistributor.PLAYER.with(() -> sp), new OpenHolographicSignEditor(pos));
         }
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos) {
+        if (state.getValue(WATERLOGGED)) {
+            level.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+        }
+        return super.updateShape(state, facing, facingState, level, currentPos, facingPos);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
     }
 }

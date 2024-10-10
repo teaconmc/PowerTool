d422a3a51c574bad0a9c726f5cc2ec6234bc1ac8
xkball <45330674+xkball@users.noreply.github.com>
2024-10-10 21:05:41 +0800

    给命令碎片GUI添加重命名功能
    Took 31 minutes

--- src/main/java/org/teacon/powertool/client/gui/SetCommandScreen.java
+++ src/main/java/org/teacon/powertool/client/gui/SetCommandScreen.java
@@ -5,11 +5,13 @@ import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.component.DataComponentPatch;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.network.PacketDistributor;
+import org.teacon.powertool.client.gui.widget.ObjectInputBox;
 import org.teacon.powertool.item.PowerToolItems;
 import org.teacon.powertool.network.server.UpdateItemStackData;
 
@@ -17,7 +19,8 @@ public class SetCommandScreen extends Screen {
     
     protected final ItemStack itemStack;
     protected final EquipmentSlot slot;
-    protected EditBox input;
+    protected ObjectInputBox<String> name;
+    protected ObjectInputBox<String> input;
     
     public SetCommandScreen(ItemStack stack, EquipmentSlot slot) {
         super(Component.translatable("powertool.setcommand.gui"));
@@ -33,16 +36,20 @@ public class SetCommandScreen extends Screen {
                 .pos(this.width / 2 - 100, this.height / 4 + 120)
                 .size(200, 20).build());
         var box_l = (int)Math.max(100,width*0.4);
-        this.input = new EditBox(font,width/2-box_l/2,height/2-35,box_l,20,Component.empty());
+        this.name = new ObjectInputBox<>(font,width/2-box_l/2,height/2-60,box_l,20,Component.literal("name"),ObjectInputBox.PASS_VALIDATOR,ObjectInputBox.PASS_RESPONDER);
+        this.name.setMaxLength(114514);
+        this.name.setRenderState(false);
+        this.input = new ObjectInputBox<>(font,width/2-box_l/2,height/2-35,box_l,20,Component.literal("command"),ObjectInputBox.PASS_VALIDATOR,ObjectInputBox.PASS_RESPONDER);
         this.input.setMaxLength(114514);
-        this.input.setFocused(false);
-        this.input.setCanLoseFocus(true);
+        this.input.setRenderState(false);
         
         String command = itemStack.get(PowerToolItems.COMMAND);
         if (command != null) {
             this.input.setValue(command);
         }
+        this.name.setValue(itemStack.getHoverName().getString());
         
+        this.addRenderableWidget(this.name);
         this.addRenderableWidget(this.input);
         super.init();
     }
@@ -55,10 +62,10 @@ public class SetCommandScreen extends Screen {
     
     @Override
     public void removed() {
-        if(input == null) return;
-        var patch = DataComponentPatch.builder()
-                .set(PowerToolItems.COMMAND.get(),input.getValue())
-                .build();
-        PacketDistributor.sendToServer(new UpdateItemStackData(slot,patch));
+        if(input == null || name == null) return;
+        var patch = DataComponentPatch.builder().set(DataComponents.CUSTOM_NAME,Component.literal(name.getValue()));
+        if(!input.getValue().isEmpty()) patch.set(PowerToolItems.COMMAND.get(),input.getValue());
+        
+        PacketDistributor.sendToServer(new UpdateItemStackData(slot,patch.build()));
     }
 }

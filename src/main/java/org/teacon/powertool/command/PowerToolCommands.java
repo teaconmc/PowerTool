82df34ce50fca96bb6b4c11e1fceafd89ccff396
xkball <45330674+xkball@users.noreply.github.com>
2026-07-12 23:37:21 +0800

    set game time command
    Took 20 minutes

--- src/main/java/org/teacon/powertool/command/PowerToolCommands.java
+++ src/main/java/org/teacon/powertool/command/PowerToolCommands.java
@@ -1,5 +1,10 @@
 package org.teacon.powertool.command;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import net.minecraft.client.Minecraft;
+import net.minecraft.commands.Commands;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.storage.PrimaryLevelData;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
@@ -14,5 +19,20 @@ public class PowerToolCommands {
         AccelerateCommand.register(event.getDispatcher());
         CreativeNoClipCommand.register(event.getDispatcher());
         FlyNoDriftCommand.register(event.getDispatcher());
+        
+        var setGameTimeCommand =
+                Commands.literal("setGameTime")
+                        .then(Commands.argument("value", IntegerArgumentType.integer(0))
+                                .executes((c) -> {
+                                    var v = IntegerArgumentType.getInteger(c,"value");
+                                    var worldData = c.getSource().getLevel().getServer().getWorldData();
+                                    if(worldData instanceof PrimaryLevelData data){
+                                        data.setGameTime(v);
+                                    }
+                                    return 1;
+                                }));
+        event.getDispatcher().register(Commands.literal("powertool").requires(Commands.hasPermission(Commands.LEVEL_OWNERS)).then(setGameTimeCommand));
+        event.getDispatcher().register(Commands.literal("pt").requires(Commands.hasPermission(Commands.LEVEL_OWNERS)).then(setGameTimeCommand));
     }
+    
 }

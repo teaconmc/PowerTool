8bc043edbd2e9ec7f7e4e8d68a8d5ce8c1a97bbf
xkball <45330674+xkball@users.noreply.github.com>
2026-07-29 00:30:35 +0800

    bezier curve block tooltip
    Took 15 minutes

--- src/main/java/org/teacon/powertool/block/BezierCurveBlock.java
+++ src/main/java/org/teacon/powertool/block/BezierCurveBlock.java
@@ -1,13 +1,17 @@
 package org.teacon.powertool.block;
 
-import com.mojang.logging.annotations.MethodsReturnNonnullByDefault;
 import com.mojang.serialization.MapCodec;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
@@ -17,14 +21,14 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.neoforged.neoforge.network.PacketDistributor;
 import org.jspecify.annotations.Nullable;
+import org.teacon.powertool.annotation.NonNullByDefault;
 import org.teacon.powertool.block.entity.BezierCurveBlockEntity;
 import org.teacon.powertool.network.client.OpenBlockScreen;
 
-import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.function.Consumer;
 
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-public class BezierCurveBlock extends BaseEntityBlock {
+@NonNullByDefault
+public class BezierCurveBlock extends BaseEntityBlock implements WithTooltip {
     
     public static final MapCodec<BezierCurveBlock> CODEC = simpleCodec(BezierCurveBlock::new);
     
@@ -63,6 +67,13 @@ public class BezierCurveBlock extends BaseEntityBlock {
     @Override
     protected int getLightDampening(BlockState state) {
         return 0;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, Item.TooltipContext context, TooltipDisplay display, Consumer<Component> builder, TooltipFlag tooltipFlag) {
+        for (int line = 1; line <= 5; line++) {
+            builder.accept(Component.translatable("block.powertool.bezier_curve_block.tooltip." + line).withStyle(ChatFormatting.DARK_GRAY));
+        }
     }
     
     @Nullable

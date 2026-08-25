f5059d9557832d62f6077309b39d17da0d75c606
zxy19 <2952795729@qq.com>
2026-08-25 21:34:30 +0800

    Refactor display mode, use blockpos from client blockhit instead of an extra clip (#50)

--- src/main/java/org/teacon/powertool/mixin/ServerPlayerMixin.java
+++ src/main/java/org/teacon/powertool/mixin/ServerPlayerMixin.java
@@ -4,40 +4,53 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.MenuProvider;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.HitResult;
 import net.neoforged.neoforge.network.PacketDistributor;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.teacon.powertool.network.client.UpdateOpenMenuSourcePacket;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.teacon.powertool.api.IServerPlayerInteractingBlockPos;
+import org.teacon.powertool.network.client.UpdateOpenMenuSourcePacket;
 
 import java.util.OptionalInt;
 import java.util.function.Consumer;
 
 @Mixin(ServerPlayer.class)
-public abstract class ServerPlayerMixin {
-    
+public abstract class ServerPlayerMixin implements IServerPlayerInteractingBlockPos {
+
+    @Unique
+    private @Nullable BlockPos powerTool$interactingBlockPos;
+
+    @Override
+    public void powerTool$startInteractingBlockPos(BlockPos pos) {
+        powerTool$interactingBlockPos = pos;
+    }
+
+    @Override
+    public void powerTool$endInteractingBlockPos() {
+        powerTool$interactingBlockPos = null;
+    }
+
+    @Override
+    public BlockPos powerTool$getInteractingBlockPos() {
+        return powerTool$interactingBlockPos;
+    }
+
     @Inject(
             method = "openMenu(Lnet/minecraft/world/MenuProvider;Ljava/util/function/Consumer;)Ljava/util/OptionalInt;",
-            at = @At("HEAD"),
-            cancellable = true
+            at = @At("HEAD")
     )
     private void onOpenMenu(MenuProvider menu, Consumer<RegistryFriendlyByteBuf> extraDataWriter, CallbackInfoReturnable<OptionalInt> cir) {
         ServerPlayer thiz = (ServerPlayer) (Object) this;
         if (thiz.getAbilities().instabuild) return;
-        HitResult hitResult = thiz.pick(thiz.blockInteractionRange(), 0, false);
-        if (hitResult != null) {
-            if (hitResult instanceof BlockHitResult blockHitResult) {
-                BlockPos pos = blockHitResult.getBlockPos();
-                PacketDistributor.sendToPlayer(
-                        thiz,
-                        new UpdateOpenMenuSourcePacket(pos)
-                );
-            }
-            return;
+        BlockPos pos = powerTool$getInteractingBlockPos();
+        if (pos != null) {
+            PacketDistributor.sendToPlayer(
+                    thiz,
+                    new UpdateOpenMenuSourcePacket(pos)
+            );
         }
-        cir.cancel();
     }
 }
